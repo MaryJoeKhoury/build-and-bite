30184afd57c4123e541c641d50adcf0604f1e6d7
Mary Joe <maryjoekhoury@outlook.com>
2023-12-10 01:50:25 +0200

    Added build a burger section

---
+++ build-burger/README.md
@@ -0,0 +1,5 @@
+# Would you like fries with that?
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/wejjwejwee/pen/KKJbjQe](https://codepen.io/wejjwejwee/pen/KKJbjQe).
+
+Build your own SVG burger
---
+++ test.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  // centeredSlides: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
