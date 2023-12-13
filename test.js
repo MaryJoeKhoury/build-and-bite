2fd27f5b2c87cfa2c44cec2d28c08bf17d72b943
Mary Joe <maryjoekhoury@outlook.com>
2023-12-13 09:40:54 +0200

    changed folder name

--- test.js
+++ test.js
@@ -43,13 +43,18 @@ const swiper = new Swiper(".swiper", {
 
 const submitForm = () => {
   const form = document.getElementById("bookingForm");
+  // lform houwe kl shi input bl bookingForm
   const formData = new FormData(form);
+  // ldata henne name email phone city.. mtl lstructure bi c++
 
   const postData = {};
   formData.forEach((value, key) => {
     postData[key] = value;
   });
 
+  // 7attein la kl item (struct) key w value lvalue hiye name email...
+
+  // hon 7atteit condition eza in case kn we7de mennoun fade=>alert opps
   if (
     !postData.name ||
     !postData.email ||
@@ -64,16 +69,21 @@ const submitForm = () => {
     });
   }
 
+  // fetch hiye ma7al ma ra7 yetsajalo lma3loumet
   fetch("http://127.0.0.1:8090/api/collections/reservations/records", {
     method: "POST",
+    // post y3ne 3am eb3at 3al backend database
     headers: {
       "Content-Type": "application/json",
     },
+    // json houwe kif 3am birou7 shakl hal data 3al backend
     body: JSON.stringify(postData),
   })
     .then((response) => response.json())
+    // bas tousal ldata 3al backend 7a yred response enno woslo ldata henne lossas li ra7it 3al backend mtl name balala
     .then((data) => {
       console.log("Response:", data);
+      // han bshouf bl inspect ldata li nba3atit w bired bi alert ahlan wa sahlan
       Swal.fire({
         title: `Welcome ${data.name} to Build & Bite 🙌!`,
         text: "Your table has been reserved 🤩!",
@@ -83,4 +93,5 @@ const submitForm = () => {
     .catch((error) => {
       console.error("Error:", error);
     });
+  // hon fi 7al tawal lresponse aw fa2a3 w ma wosil then byenrad error
 };
